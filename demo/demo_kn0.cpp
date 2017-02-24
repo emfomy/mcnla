684b56103782ef7eccf35764ec1014f6d44ffbfb
Mu Yang <emfomy@gmail.com>
2017-02-24 09:56:52 +0800

    Fix typo in MatrixMarket IO.

--- demo/demo_kn0.cpp
+++ demo/demo_kn0.cpp
@@ -29,7 +29,7 @@ int main( int argc, char **argv ) {
     std::cout << "MCNLA "
               << MCNLA_MAJOR_VERSION << "."
               << MCNLA_MINOR_VERSION << "."
-              << MCNLA_PATCH_VERSION << " KN demo" << std::endl << std::endl;
+              << MCNLA_PATCH_VERSION << " Naive KN demo" << std::endl << std::endl;
   }
 
   // ====================================================================================================================== //
