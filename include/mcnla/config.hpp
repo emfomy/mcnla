80e31fcc7887cdcf1638b4c0ec38455829ebfc4b
Mu Yang <emfomy@gmail.com>
2017-03-28 16:31:10 +0800

    Recover main headers.

---
+++ include/mcnla/config.hpp
@@ -0,0 +1,15 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// @file    include/mcnla/config.hpp
+/// @brief   The configured options and settings.
+///
+/// @author  Mu Yang <<emfomy@gmail.com>>
+///
+
+#ifndef MCNLA_CONFIG_HPP_
+#define MCNLA_CONFIG_HPP_
+
+#define MCNLA_MAJOR_VERSION @MCNLA_MAJOR_VERSION@ ///< The major version of MCNLA.
+#define MCNLA_MINOR_VERSION @MCNLA_MINOR_VERSION@ ///< The minor version of MCNLA.
+#define MCNLA_PATCH_VERSION @MCNLA_PATCH_VERSION@ ///< The patch version of MCNLA.
+
+#endif  // MCNLA_CONFIG_HPP_
