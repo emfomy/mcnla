2cb72f21da12c036fccdbccc1b3eb98391ead940
Mu Yang <emfomy@gmail.com>
2017-04-21 12:09:03 +0800

    Bug fix.

--- demo/test.cpp
+++ demo/test.cpp
@@ -81,8 +81,10 @@ int main( int argc, char **argv ) {
     std::cout << "Uses " << former << "." << std::endl << std::endl;
   }
 
-  sketcher(aj, qij);;
+  sketcher(aj, qij);
+  so_converter(qij, qi);
   orthogonalizer(qi);
+  oi_converter(qi, qij);
   integrator(qij, qbarj);
   if_converter(qbarj, qbar);
   former(a, qbar);
