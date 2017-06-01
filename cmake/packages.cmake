5550be46ae4cb5d0d1e7011f4fb450eb4ceff10d
Mu Yang <emfomy@gmail.com>
2017-06-01 10:38:57 +0800

    Update CMake files.

--- cmake/packages.cmake
+++ cmake/packages.cmake
@@ -1,64 +1,68 @@
+# Set complier flags
+set(CMAKE_CXX_FLAGS "-std=c++11 -O3 -g -Wall -Wextra -pedantic")
+# set(CMAKE_CXX_FLAGS "-std=c++11 -O0 -g -fsanitize=address -Wall -Wextra -pedantic")
+
 if(MCNLA_BUILD_BIN)
-  # Set default variables
-  set(INCS "")
-  set(LIBS "")
-  set(COMFLGS "")
-  set(LNKFLGS "")
+  set(findtype REQUIRED)
+else()
+  set(findtype "")
+endif()
 
-  # Set complier flags
-  set(CMAKE_CXX_FLAGS "-std=c++11 -O3 -g -Wall -Wextra -pedantic")
-  # set(CMAKE_CXX_FLAGS "-std=c++11 -O0 -g -fsanitize=address -Wall -Wextra -pedantic")
+# Set default variables
+set(INCS "")
+set(LIBS "")
+set(COMFLGS "")
+set(LNKFLGS "")
 
-  # MPI
-  find_package(MPI REQUIRED)
-  if(MPI_FOUND)
-    list(APPEND INCS "${MPI_INCLUDE_PATH}")
-    list(APPEND LIBS "${MPI_LIBRARIES}")
-    set(COMFLGS "${COMFLGS} ${MPI_COMPILE_FLAGS}")
-    set(LNKFLGS "${LNKFLGS} ${MPI_LINK_FLAGS}")
+# MPI
+find_package(MPI ${findtype})
+if(MPI_FOUND)
+  list(APPEND INCS "${MPI_INCLUDE_PATH}")
+  list(APPEND LIBS "${MPI_LIBRARIES}")
+  set(COMFLGS "${COMFLGS} ${MPI_COMPILE_FLAGS}")
+  set(LNKFLGS "${LNKFLGS} ${MPI_LINK_FLAGS}")
+endif()
+
+# MKL
+if(MCNLA_USE_MKL)
+  find_package(MKL ${findtype})
+  if(MKL_FOUND)
+    list(APPEND INCS "${MKL_INCLUDES}")
+    list(APPEND LIBS "${MKL_LIBRARIES}")
+    set(COMFLGS "${COMFLGS} ${MKL_FLAGS}")
+  endif()
+endif()
+
+# LAPACK
+if(NOT MCNLA_USE_MKL)
+  find_package(LAPACK ${findtype})
+  if(LAPACK_FOUND)
+    list(APPEND LIBS "${LAPACK_LIBRARIES}")
+  endif()
+endif()
+
+# OpenMP
+if(MCNLA_OMP)
+  set(OpenMP ${MCNLA_OMP})
+
+  find_package(OpenMP ${findtype})
+  if(OpenMP_FOUND)
+    set(COMFLGS "${COMFLGS} ${OpenMP_CXX_FLAGS}")
+    set(LNKFLGS "${LNKFLGS} ${OpenMP_CXX_FLAGS}")
   endif()
 
-  # MKL
-  if(MCNLA_USE_MKL)
-    find_package(MKL REQUIRED)
-    if(MKL_FOUND)
-      list(APPEND INCS "${MKL_INCLUDES}")
-      list(APPEND LIBS "${MKL_LIBRARIES}")
-      set(COMFLGS "${COMFLGS} ${MKL_FLAGS}")
-    endif()
+  find_package(OpenMPLib ${findtype})
+  if(OpenMPLib_FOUND)
+    list(APPEND LIBS "${OpenMP_LIBRARIES}")
   endif()
 
-  # LAPACK
-  if(NOT MCNLA_USE_MKL)
-    find_package(LAPACK REQUIRED)
-    if(LAPACK_FOUND)
-      list(APPEND LIBS "${LAPACK_LIBRARIES}")
-    endif()
-  endif()
-
-  # OpenMP
-  if(MCNLA_OMP)
-    set(OpenMP ${MCNLA_OMP})
-
-    find_package(OpenMP REQUIRED)
-    if(OpenMP_FOUND)
-      set(COMFLGS "${COMFLGS} ${OpenMP_CXX_FLAGS}")
-      set(LNKFLGS "${LNKFLGS} ${OpenMP_CXX_FLAGS}")
-    endif()
-
-    find_package(OpenMPLib REQUIRED)
-    if(OpenMPLib_FOUND)
-      list(APPEND LIBS "${OpenMP_LIBRARIES}")
-    endif()
-
-    unset(OpenMP)
-  endif()
+  unset(OpenMP)
 endif()
 
 # CUDA & MAGMA
 # if(MCNLA_USE_GPU)
-#   find_package(CUDA REQUIRED)
-#   find_package(MAGMA REQUIRED)
+#   find_package(CUDA ${findtype})
+#   find_package(MAGMA ${findtype})
 #   if(MAGMA_FOUND)
 #     list(APPEND INCS "${MAGMA_INCLUDES}")
 #     list(APPEND LIBS "${MAGMA_LIBRARIES}" "${CUDA_CUDART_LIBRARY}" "${CUDA_cublas_LIBRARY}" "${CUDA_cusparse_LIBRARY}")
