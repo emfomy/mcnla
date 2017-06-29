b662c24a32c917433d031b4083622d3dc1df3abb
Mu Yang <emfomy@gmail.com>
2017-06-29 11:54:21 +0800

    Update report codes.

--- report/include/stage.hpp
+++ report/include/stage.hpp
@@ -72,7 +72,8 @@ int main( int argc, char **argv ) {
 #else  // _OPENMP
               << 1
 #endif  // _OPENMP
-              << " threads per node" << std::endl << std::endl;
+              << " threads per node" << std::endl;
+    std::cout << sizeof(mcnla::index_t)*8 << "bit integer" << std::endl << std::endl;
   }
 
   StageType stage(parameters);
